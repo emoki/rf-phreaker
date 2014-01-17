d927fe53992a79ada56534a42f3e64a3e0a043df
ethan steffens <ethan.steffens@gmail.com>
2014-01-17 13:14:28 -0500

    Changed lte_layer_3_decoder import/export to work with GCC.

--- src/rf_phreaker/lte_layer_3_decoder/lte_layer_3_decoder.h
+++ src/rf_phreaker/lte_layer_3_decoder/lte_layer_3_decoder.h
@@ -1,13 +1,26 @@
-// The following ifdef block is the standard way of creating macros which make exporting 
-// from a DLL simpler. All files within this DLL are compiled with the ASN1C_OPEN_SOURCE_DLL_EXPORTS
-// symbol defined on the command line. This symbol should not be defined on any project
-// that uses this DLL. This way any other project whose source files include this file see 
-// ASN1C_OPEN_SOURCE_DLL_API functions as being imported from a DLL, whereas this DLL sees symbols
-// defined with this macro as being exported.
-#ifdef LTE_LAYER_3_DECODER_EXPORTS
-#define LTE_LAYER_3_DECODER_API __declspec(dllexport)
+#if defined _WIN32 || defined __CYGWIN__
+#ifdef BUILDING_DLL
+#ifdef __GNUC__
+#define DLL_PUBLIC __attribute__ ((dllexport))
 #else
-#define LTE_LAYER_3_DECODER_API __declspec(dllimport)
+#define DLL_PUBLIC __declspec(dllexport) // Note: actually gcc seems to also supports this syntax.
+#endif
+#else
+#ifdef __GNUC__
+#define DLL_PUBLIC __attribute__ ((dllimport))
+#else
+#define DLL_PUBLIC __declspec(dllimport) // Note: actually gcc seems to also supports this syntax.
+#endif
+#endif
+#define DLL_LOCAL
+#else
+#if __GNUC__ >= 4
+#define DLL_PUBLIC __attribute__ ((visibility ("default")))
+#define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+#else
+#define DLL_PUBLIC
+#define DLL_LOCAL
+#endif
 #endif
 
 #include <stdint.h>
@@ -18,7 +31,7 @@ class lte_bcch_dl_sch_message;
 
 
 
-class LTE_LAYER_3_DECODER_API lte_asn1_decoder
+class DLL_PUBLIC lte_asn1_decoder
 {
 public:
 	lte_asn1_decoder();
