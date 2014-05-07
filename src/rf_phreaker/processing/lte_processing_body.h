66d4a9ad06d4f9b10f936b1a07014d16b2ab2499
ethan steffens <ethan.steffens@gmail.com>
2014-05-06 21:36:30 -0400

    Changed additional LTE processing parameters, additional LTE logging, addtional LTE valid measurement processing.

--- src/rf_phreaker/processing/lte_processing_body.h
+++ src/rf_phreaker/processing/lte_processing_body.h
@@ -3,6 +3,7 @@
 #include "rf_phreaker/processing/layer_3_tracker.h"
 #include "rf_phreaker/lte_analysis/lte_analysis.h"
 #include "rf_phreaker/common/settings.h"
+#include "rf_phreaker/common/log.h"
 #include "tbb/flow_graph.h"
 
 namespace rf_phreaker { namespace processing {
@@ -23,7 +24,6 @@ public:
 	layer_3_settings layer_3_;
 };
 
-int count = 0;
 class lte_processing_body
 {
 public:
@@ -44,8 +44,12 @@ public:
 
 		double avg_rms = ipp_helper::calculate_average_rms(info->get_iq().get(), info->get_iq().length());
 
+		if(meas.size()) {
+			LOG_L(VERBOSE) << "LTE processing - Found " << meas.size() << " possible LTE measurments.  Frequency: " << info->frequency() / 1e6
+				<< "mhz | Bandwidth: " << info->bandwidth() / 1e6 << "mhz | Sampling rate: " << info->sampling_rate() / 1e6 << "mhz.";
+		}
+
 		return lte_info(info, std::move(meas), avg_rms);
-	
 	}
 
 	lte_info operator()(lte_info info)
@@ -63,26 +67,25 @@ public:
 			if(status != 0)
 				throw lte_analysis_error("Error decoding lte layer 3.");
 
-
 			for(auto &data : info.processed_data_) {
-				if(data.Bandwidth == LteBandwidth_5MHZ && data.NumAntennaPorts != LteAntPorts_Unknown && data.rsrp > .001) {
+				if(is_valid_measurement(data)) {
 					if((!tracker_.is_fully_decoded(freq, data) && (data.sync_quality > config_.layer_3_.decode_threshold_ || tracker_.in_history(freq, data)))) {
 						tracker_.update(freq, data);
 					}
 				}
+			}
 
-				// If no measurements were greater than the decode_threshold and we are not tracking any cells on this freq, add the cell with the greatest ecio if
-				// it meets the min decode threshold.
-				if(!tracker_.is_freq_in_history(freq)) {
-					lte_measurement meas;
-					double tmp_sync_quality = -99;
-					for(auto &data : info.processed_data_) {
-						if(data.sync_quality > tmp_sync_quality)
-							meas = data;
-					}
-					if(meas.sync_quality > config_.layer_3_.decode_minimum_threshold_)
-						tracker_.update(freq, meas);
-				}
+			// If no measurements were greater than the decode_threshold and we are not tracking any cells on this freq, add the cell with the greatest ecio if
+			// it meets the min decode threshold.
+			if(!tracker_.is_freq_in_history(freq)) {
+				lte_measurement meas;
+				double tmp_sync_quality = -99;
+				for(auto &data : info.processed_data_) {
+					if(data.sync_quality > tmp_sync_quality)
+						meas = data;
+				} // Temp checks.
+				if(meas.sync_quality > config_.layer_3_.decode_minimum_threshold_ && is_valid_measurement(meas))
+					tracker_.update(freq, meas);
 			}
 		}
 
@@ -97,7 +100,78 @@ public:
 		return static_cast<int>(nano_secs / 1e6 / 5);
 	}
 
+	bool constains_valid_measurement(lte_measurements &data)
+	{
+		bool valid = false;
+		for(auto &meas : data) {
+			if(is_valid_measurement(meas)) {
+				valid = true;
+				break;
+			}
+		}
+		return valid;
+	}
+
+	bool is_valid_measurement(lte_measurement &meas)
+	{
+		bool rsrp = meas.rsrp > .001 && (meas.rsrp <= DBL_MAX && meas.rsrp >= -DBL_MAX);
+		bool rsrq = meas.rsrq <= DBL_MAX && meas.rsrq >= -DBL_MAX;
+		bool rssi = meas.rssi > .001 && (meas.rssi <= DBL_MAX && meas.rssi >= -DBL_MAX);
+		return (meas.Bandwidth == LteBandwidth_5MHZ || meas.Bandwidth == LteBandwidth_10MHZ) && meas.NumAntennaPorts != LteAntPorts_Unknown
+			&& rsrp && rsrq && rssi;
+	}
+
 private:
+	void output(const lte_info &info)
+	{
+		static std::string timestamp;
+		if(timestamp.empty())
+			timestamp = std::to_string(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()));
+
+		LteChannelBandwidth bw = LteBandwidth_Unknown;
+		for(auto &data : info.processed_data_) {
+			if(data.Bandwidth != LteBandwidth_Unknown && data.NumAntennaPorts != LteAntPorts_Unknown && data.rsrp > .001) {
+				if(data.Bandwidth > bw)
+					bw = data.Bandwidth;
+			}
+		}
+
+		if(info.meas_->bandwidth() == mhz(5) || info.meas_->bandwidth() == mhz(10) || info.meas_->bandwidth() == mhz(15) || info.meas_->bandwidth() == mhz(20)) {
+			switch(bw) {
+			case LteBandwidth_5MHZ:
+			{
+				static int c_5 = 0;
+				std::ofstream file("lte_5mhz_" + timestamp + "_" + std::to_string(c_5++) + ".bin");
+				file << *info.meas_;
+			}
+				break;
+			case LteBandwidth_10MHZ:
+			{
+				static int c_10 = 0;
+				std::ofstream file("lte_10mhz_" + timestamp + "_" + std::to_string(c_10++) + ".bin");
+				file << *info.meas_;
+			}
+				break;
+			case LteBandwidth_15MHZ:
+			{
+				static int c_15 = 0;
+				std::ofstream file("lte_15mhz_" + timestamp + "_" + std::to_string(c_15++) + ".bin");
+				file << *info.meas_;
+			}
+				break;
+			case LteBandwidth_20MHZ:
+			{
+				static int c_20 = 0;
+				std::ofstream file("lte_20mhz_" + timestamp + "_" + std::to_string(c_20++) + ".bin");
+				file << *info.meas_;
+			}
+				break;
+			default:
+				;
+			}
+		}
+	}
+	
 	lte_processing_settings config_;
 	lte_layer_3_tracker tracker_;
 	lte_analysis analysis_;
