8a980a9441a97156db7be8cb077a38960ee401e3
ethan steffens <ethan.steffens@gmail.com>
2014-05-12 13:08:05 -0400

    Bug fix.  EARFCNs were calculated wrong because we were using the wrong start freq.  To fix we changed the LTE operating band ranges.

--- src/rf_phreaker/common/operating_band_range_specifier.cpp
+++ src/rf_phreaker/common/operating_band_range_specifier.cpp
@@ -25,6 +25,7 @@ operating_band_range_specifier::operating_band_range_specifier(void)
 	operating_bands_.push_back(operating_band_range(GSM_DCS_1800, khz(1805200), khz(1879800)));
 	operating_bands_.push_back(operating_band_range(GSM_PCS_1900, khz(1930200), khz(1989800)));
 
+	// Channels are only valid to +- 2.4khz from the end of the band.
 	operating_bands_.push_back(operating_band_range(UMTS_OPERATING_BAND_1, khz(2112400LL), khz(2167600LL)));	//  2100	IMT	1920 - 1980	2110 – 2170
 	operating_bands_.push_back(operating_band_range(UMTS_OPERATING_BAND_2, khz(1932400), khz(1987600)));	//  1900	PCS A-F	1850 – 1910	1930 – 1990
 	operating_bands_.push_back(operating_band_range(UMTS_OPERATING_BAND_3, khz(1807400), khz(1877600)));	//	1800	DCS	1710 – 1785	1805 – 1880
@@ -46,47 +47,47 @@ operating_band_range_specifier::operating_band_range_specifier(void)
 	operating_bands_.push_back(operating_band_range(UMTS_OPERATING_BAND_25, khz(1932400), khz(1992600)));	//	1900	EPCS A-G	1850 – 1915	1930 – 1995
 	operating_bands_.push_back(operating_band_range(UMTS_OPERATING_BAND_26, khz(861400), khz(891600)));	//	850	ECLR	814 – 849	859 – 894
 
-	// For the edges of the band we use the smallest possible bandwidth: 1.4 mhz.
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_1, khz(2110000LL + 1400), khz(2170000LL - 1400)));	//	1920 - 1980	2110 - 2170	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_2, khz(1930000 + 1400), khz(1990000 - 1400)));	//	1850 - 1910	1930 - 1990	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_3, khz(1805000 + 1400), khz(1880000 - 1400)));	//	1710 - 1785	1805 - 1880	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_4, khz(2110000LL + 1400), khz(2155000LL - 1400)));	//	1710 - 1755	2110 - 2155	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_5, khz(869000  + 1400), khz(894000 - 1400)));	//	824 - 849	869 - 894	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_6, khz(875000 + 1400), khz(885000 - 1400)));	//	830 - 840	875 - 885	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_7, khz(2620000LL + 1400), khz(2690000LL - 1400)));	//	2500 - 2570	2620 - 2690	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_8, khz(925000 + 1400), khz(960000 - 1400)));	//	880 - 915	925 - 960	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_9, khz(1844900 + 1400), khz(1879900 - 1400)));	//	1749.9 - 1784.9	1844.9 - 1879.9	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_10, khz(2110000LL + 1400), khz(2170000LL - 1400)));	//	1710 - 1770	2110 - 2170	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_11, khz(1475900 + 1400), khz(1495900 - 1400)));	//	1427.9 - 1447.9	1475.9 - 1495.9	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_12, khz(729000 + 1400), khz(746000 - 1400)));	//	699 - 716	729 - 746	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_13, khz(746000 + 1400), khz(756000 - 1400)));	//	777 - 787	746 - 756	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_14, khz(758000 + 1400), khz(768000 - 1400)));	//	788 - 798	758 - 768	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_17, khz(734000 + 1400), khz(746000 - 1400)));	//	704 - 716	734 - 746	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_18, khz(860000 + 1400), khz(875000 - 1400)));	//	815 - 830	860 - 875	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_19, khz(875000 + 1400), khz(890000 - 1400)));	//	830 - 845	875 - 890	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_20, khz(791000 + 1400), khz(821000 - 1400)));	//	832 - 862	791 - 821	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_21, khz(1495900 + 1400), khz(1510900 - 1400)));	//	1447.9 - 1462.9	1495.9 - 1510.9	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_22, khz(3510000LL + 1400), khz(3590000LL - 1400)));	//	3410 - 3490	3510 - 3590	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_23, khz(2180000LL + 1400), khz(2200000LL - 1400)));	//	2000 - 2020	2180 - 2200	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_24, khz(1525000 + 1400), khz(1559000 - 1400)));	//	1626.5 - 1660.5	1525 - 1559	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_25, khz(1930000 + 1400), khz(1995000 - 1400)));	//	1850 - 1915	1930 - 1995	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_26, khz(859000 + 1400), khz(894000 - 1400)));	//	814 - 849	859 - 894	FDD			
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_27, khz(852000 + 1400), khz(869000 - 1400)));	//	807 - 824	852 - 869	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_28, khz(758000 + 1400), khz(803000 - 1400)));	//	703 - 748	758 - 803	FDD
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_29, khz(717000 + 1400), khz(728000 - 1400)));	//	N/A	716 - 728	FDD		
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_30, khz(2350000LL + 1400), khz(2360000LL - 1400)));	//	2305 - 2315 	2350 - 2360	FDD		
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_33, khz(1900000 + 1400), khz(1920000 - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_34, khz(2010000LL + 1400), khz(2025000LL - 1400)));	//
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_35, khz(1850000 + 1400), khz(1910000 - 1400)));	//
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_36, khz(1930000 + 1400), khz(1990000 - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_37, khz(1910000 + 1400), khz(1930000 - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_38, khz(2570000LL + 1400), khz(2620000LL - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_39, khz(1880000 + 1400), khz(1920000 - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_40, khz(2300000LL + 1400), khz(2400000LL - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_41, khz(2496000LL + 1400), khz(2690000LL - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_42, khz(3400000LL + 1400), khz(3600000LL - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_43, khz(3600000LL + 1400), khz(3800000LL - 1400)));	//	
-	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_44, khz(703000 + 1400), khz(803000 - 1400)));	//	
+	// Channels are valid all the way to the edges of the band.  
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_1, khz(2110000LL), khz(2170000LL)));	//	1920 - 1980	2110 - 2170	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_2, khz(1930000), khz(1990000)));	//	1850 - 1910	1930 - 1990	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_3, khz(1805000), khz(1880000)));	//	1710 - 1785	1805 - 1880	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_4, khz(2110000LL), khz(2155000LL)));	//	1710 - 1755	2110 - 2155	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_5, khz(869000 ), khz(894000)));	//	824 - 849	869 - 894	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_6, khz(875000), khz(885000)));	//	830 - 840	875 - 885	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_7, khz(2620000LL), khz(2690000LL)));	//	2500 - 2570	2620 - 2690	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_8, khz(925000), khz(960000)));	//	880 - 915	925 - 960	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_9, khz(1844900), khz(1879900)));	//	1749.9 - 1784.9	1844.9 - 1879.9	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_10, khz(2110000LL), khz(2170000LL)));	//	1710 - 1770	2110 - 2170	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_11, khz(1475900), khz(1495900)));	//	1427.9 - 1447.9	1475.9 - 1495.9	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_12, khz(729000), khz(746000)));	//	699 - 716	729 - 746	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_13, khz(746000), khz(756000)));	//	777 - 787	746 - 756	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_14, khz(758000), khz(768000)));	//	788 - 798	758 - 768	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_17, khz(734000), khz(746000)));	//	704 - 716	734 - 746	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_18, khz(860000), khz(875000)));	//	815 - 830	860 - 875	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_19, khz(875000), khz(890000)));	//	830 - 845	875 - 890	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_20, khz(791000), khz(821000)));	//	832 - 862	791 - 821	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_21, khz(1495900), khz(1510900)));	//	1447.9 - 1462.9	1495.9 - 1510.9	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_22, khz(3510000LL), khz(3590000LL)));	//	3410 - 3490	3510 - 3590	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_23, khz(2180000LL), khz(2200000LL)));	//	2000 - 2020	2180 - 2200	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_24, khz(1525000), khz(1559000)));	//	1626.5 - 1660.5	1525 - 1559	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_25, khz(1930000), khz(1995000)));	//	1850 - 1915	1930 - 1995	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_26, khz(859000), khz(894000)));	//	814 - 849	859 - 894	FDD			
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_27, khz(852000), khz(869000)));	//	807 - 824	852 - 869	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_28, khz(758000), khz(803000)));	//	703 - 748	758 - 803	FDD
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_29, khz(717000), khz(728000)));	//	N/A	716 - 728	FDD		
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_30, khz(2350000LL), khz(2360000LL)));	//	2305 - 2315 	2350 - 2360	FDD		
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_33, khz(1900000), khz(1920000)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_34, khz(2010000LL), khz(2025000LL)));	//
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_35, khz(1850000), khz(1910000)));	//
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_36, khz(1930000), khz(1990000)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_37, khz(1910000), khz(1930000)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_38, khz(2570000LL), khz(2620000LL)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_39, khz(1880000), khz(1920000)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_40, khz(2300000LL), khz(2400000LL)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_41, khz(2496000LL), khz(2690000LL)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_42, khz(3400000LL), khz(3600000LL)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_43, khz(3600000LL), khz(3800000LL)));	//	
+	operating_bands_.push_back(operating_band_range(LTE_OPERATING_BAND_44, khz(703000), khz(803000)));	//	
 }
 
 
