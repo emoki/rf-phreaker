d415fb6fd2b01724c59b4000aa6ca65c0ef4223f
ethan steffens <ethan.steffens@gmail.com>
2014-05-16 18:34:39 -0400

    Squashed 'third_party/bladerf/' changes from 91ef6d4..ea93345
    ea93345 Removing assertion and returning error value instread so that the application will not crash when using bladerf_sync_rx.
    2dd6993 Merge branch 'upstream'
    b870945 Merge branch 'master' of https://github.com/Nuand/bladeRF into upstream
    2a90a56 host: Added additional configuration-time libusb function checks
    991f1da libbladeRF: Moved internal/external sampling logic to lms.c
    e075cc6 bladeRF-cli: Removed python references and build dependencies
    273760a bladeRF-cli: Initialized value for help-interactive option flag
    aebee6e bladeRF-cli: Reformatted help text
    2ba30c6 bladeRF-cli, doc: Use --help-interactive to get cmds from bladeRF-cli
    3708dcf bladeRF-cli: Added --help-interactive command line argument
    
    git-subtree-dir: third_party/bladerf
    git-subtree-split: ea93345a21bace2d16e30f7059f29a9519a760ad

--- host/libraries/libbladeRF/src/backend/usb/libusb.c
+++ host/libraries/libbladeRF/src/backend/usb/libusb.c
@@ -739,7 +739,11 @@ static int submit_transfer(struct bladerf_stream *stream, void *buffer)
     const unsigned char ep =
         stream->module == BLADERF_MODULE_TX ? SAMPLE_EP_OUT : SAMPLE_EP_IN;
 
-    assert(stream_data->transfer_status[stream_data->i] == TRANSFER_AVAIL);
+	// Ethan Steffens - Instead of asserting we return LIBUSB_ERROR_OTHER.  This allows us to 
+	// reinitialize sync_rx and continue transferring data.
+    if(stream_data->transfer_status[stream_data->i] != TRANSFER_AVAIL)
+		return error_conv(-99);
+
     transfer = stream_data->transfers[stream_data->i];
 
     switch (stream->format) {
