747eda90b5051af4d2479287febc4c011af6fc94
ethan steffens <ethan.steffens@gmail.com>
2014-06-20 11:29:18 -0400

    Added additional calibration utilities.

---
+++ src/rf_phreaker/general_utility/calibration_utility.h
@@ -0,0 +1,159 @@
+#pragma once
+
+#include <string>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+#include "rf_phreaker/scanner/blade_rf_controller.h"
+
+class calibration_utility
+{
+public:
+	static void read_calibration_and_output(const std::string &nuand_calibration_filename, const std::string &rf_board_calibration_filename,
+		const std::string &switch_setting_filename) {
+		using namespace rf_phreaker::scanner;
+
+		try {
+			calibration cal;
+			cal.read_nuand_calibration_file(nuand_calibration_filename);
+			cal.read_rf_board_calibration_file(rf_board_calibration_filename);
+			cal.read_rf_switch_file(switch_setting_filename);
+
+			{
+				std::string cali_filename = cal.nuand_serial_ + "_cali.bin";
+				std::ofstream f(cali_filename, std::ios::binary);
+				if(!f)
+					throw rf_phreaker::rf_phreaker_error("Unable to open calibration file (" + cali_filename + ").");
+				boost::archive::binary_oarchive ia(f);
+				ia & cal;
+			}
+		}
+		catch(rf_phreaker::rf_phreaker_error &err) {
+			std::cout << "Error: " << err.what() << std::endl;
+		}
+		catch(std::exception &err) {
+			std::cout << "Error: " << err.what() << std::endl;
+		}
+		catch(...) {
+			std::cout << "Unknown error has occurred." << std::endl;
+		}
+	}
+
+	static void load_calibrations(const std::vector<rf_phreaker::scanner::scanner_id_type> &list) {
+		using namespace rf_phreaker::scanner;
+
+
+		try {
+			std::cout << "Searching for the following scanner(s):\n";
+			for(auto serial : list)
+				std::cout << "   " << serial << "\n";
+
+			std::vector<comm_info_ptr> scanners;
+			{
+				blade_rf_controller controller;
+				scanners = controller.list_available_scanners();
+				if(scanners.size() == 0)
+					throw rf_phreaker::rf_phreaker_error("No scanners detected.");
+				else
+					std::cout << "\nFound " << scanners.size() << " scanner(s) connected.\n";
+			}
+
+			bool found_serial = false;
+			for(auto serial : list) {
+				auto scanner = std::find_if(scanners.begin(), scanners.end(), [&](comm_info_ptr &comm) {
+					return comm->id() == serial;
+				});
+
+				if(scanner == scanners.end())
+					continue;
+				else {
+					std::cout << "\nFound scanner " << serial << "\n";
+					found_serial = true;
+					load_calibration(serial, serial + "_cali.bin");
+				}
+			}
+
+			if(!found_serial)
+				std::cout << "\nNo specified scanner(s) detected.\n";
+		}
+		catch(rf_phreaker::rf_phreaker_error &err) {
+			std::cout << "\nError: " << err.what() << std::endl;
+		}
+		catch(std::exception &err) {
+			std::cout << "\nError: " << err.what() << std::endl;
+		}
+		catch(...) {
+			std::cout << "\nUnknown error has occurred." << std::endl;
+		}
+
+	}
+
+	static void load_calibration(const rf_phreaker::scanner::scanner_id_type &serial, const std::string &cali_filename) {
+		using namespace rf_phreaker::scanner;
+
+		try {
+			std::cout << "\nLoading calibration...\n";
+
+			calibration cal = read_calibration_from_file(cali_filename);
+
+			blade_rf_controller controller;
+
+			auto scanners = controller.list_available_scanners();
+
+			auto scanner = std::find_if(scanners.begin(), scanners.end(), [&](comm_info_ptr &comm) {
+				return comm->id() == serial;
+			});
+
+			if(scanner == scanners.end())
+				throw rf_phreaker::rf_phreaker_error("Scanner " + serial + " was not detected.");
+			else
+				std::cout << "\nOpening scanner " << serial << ".\n";
+
+			controller.open_scanner(serial);
+
+			if(!controller.read_eeprom_meta_data().is_valid()) {
+				std::cout << "\nInitializing EEPROM.\n";
+				controller.initialize_eeprom();
+			}
+			else {
+				// If the EEPROM is valid then we use the freq correction value and date. 
+				auto tmp_cal = controller.read_calibration();
+				if(tmp_cal.nuand_freq_correction_value_ > 0 && tmp_cal.nuand_freq_correction_date_ > 0) {
+					cal.nuand_freq_correction_value_ = tmp_cal.nuand_freq_correction_value_;
+					cal.nuand_freq_correction_date_ = tmp_cal.nuand_freq_correction_date_;
+				}
+			}
+			std::cout << "\nWriting calibration to EEPROM.\n";
+			controller.write_calibration(cal);
+
+			std::cout << "\nCalibration loaded successfully!\n";
+			return;
+		}
+		catch(rf_phreaker::rf_phreaker_error &err) {
+			std::cout << "\nError: " << err.what() << std::endl;
+		}
+		catch(std::exception &err) {
+			std::cout << "\nError: " << err.what() << std::endl;
+		}
+		catch(...) {
+			std::cout << "\nUnknown error has occurred." << std::endl;
+		}
+
+		std::cout << "\nLoading calibration failed!" << std::endl;
+	}
+
+	static rf_phreaker::scanner::calibration read_calibration_from_file(const std::string &filename)
+	{
+		using namespace rf_phreaker::scanner;
+
+		std::cout << "\nReading calibration from file.\n";
+
+		calibration cal;
+		std::ifstream f(filename, std::ios::binary);
+		if(!f)
+			throw rf_phreaker::rf_phreaker_error("Unable to open calibration file (" + filename + ").");
+		boost::archive::binary_iarchive ia(f);
+		ia & cal;
+		
+		return cal;
+	}
+};
--- src/rf_phreaker/general_utility/general_utility.cpp
+++ src/rf_phreaker/general_utility/general_utility.cpp
@@ -1,12 +1,68 @@
 #include "stdafx.h"
-#include "rf_phreaker/general_utility/calibration.h"
+#include <boost/program_options.hpp>
+#include "rf_phreaker/general_utility/calibration_utility.h"
 
 
 int main(int argc, char* argv[]) {
+	namespace po = boost::program_options;
 
-	load_calibration("59b28129e0157100d341b3297caf2081", "59b28129e0157100d341b3297caf2081_cali.bin");
-	//load_calibration("77ecda454d25738ee419f6fd676170d5", "59b28129e0157100d341b3297caf2081_cali.bin");
+	try {
+		// Option variables.
+		std::vector<std::string> serials;
 
+		po::options_description desc("Allowed options");
+
+		desc.add_options()
+			("help,h", "Produce help message.")
+			("upload_calibration,c", po::value<std::vector<std::string>>(&serials)->multitoken(),
+			"Uploads a calibration file to a scanner.\n"
+			"Calibration filename format:\n"
+			"13cadd7137a3ef4d18dcfcc179667998_cali.bin."
+			"\nExample usage:\n"
+			"'-c 13cadd7137a3ef4d18dcfcc179667998'"
+			);
+
+		// Read command line.
+		po::variables_map vm;
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+		if(vm.count("help")) {
+			std::cout << desc << std::endl;
+			return 1;
+		}
+		vm.notify();
+
+		calibration_utility::load_calibrations(serials);
+
+
+		//auto tmp1 = calibration_utility::read_calibration_from_file("13cadd7137a3ef4d18dcfcc179667998_cali.bin");
+		//auto tmp2 = calibration_utility::read_calibration_from_file("47d54d57db30c9169c98c53e30c08d9a_cali.bin");
+		//auto tmp3 = calibration_utility::read_calibration_from_file("d01d12c0dc5c71c8a081e0c25f27b6fd_cali.bin");
+		//auto tmp4 = calibration_utility::read_calibration_from_file("d7db1c90fd06a5a6d950615ea7fa6164_cali.bin");
+		//auto tmp5 = calibration_utility::read_calibration_from_file("77ecda454d25738ee419f6fd676170d5_cali.bin");
+
+
+		//std::string base = "../../../../rf_phreaker/test_files/calibration_files/";
+
+		//calibration_utility::read_calibration_and_output(base + "Nuand_Board_13cadd7137a3ef4d18dcfcc179667998_140605.txt", base + "RF_Board_13cadd7137a3ef4d18dcfcc179667998_140605.txt",
+		//	base + "RF_Switch_13cadd7137a3ef4d18dcfcc179667998_140605.txt");
+
+		//calibration_utility::read_calibration_and_output(base + "Nuand_Board_47d54d57db30c9169c98c53e30c08d9a_140605.txt", base + "RF_Board_47d54d57db30c9169c98c53e30c08d9a_140605.txt",
+		//	base + "RF_Switch_47d54d57db30c9169c98c53e30c08d9a_140605.txt");
+
+		//calibration_utility::read_calibration_and_output(base + "Nuand_Board_d01d12c0dc5c71c8a081e0c25f27b6fd_140605.txt", base + "RF_Board_d01d12c0dc5c71c8a081e0c25f27b6fd_140605.txt",
+		//	base + "RF_Switch_d01d12c0dc5c71c8a081e0c25f27b6fd_140605.txt");
+
+		//calibration_utility::read_calibration_and_output(base + "Nuand_Board_d7db1c90fd06a5a6d950615ea7fa6164_140605.txt", base + "RF_Board_d7db1c90fd06a5a6d950615ea7fa6164_140605.txt",
+		//	base + "RF_Switch_d7db1c90fd06a5a6d950615ea7fa6164_140605.txt");
+	}
+	catch(std::exception &err) {
+		std::cout << "Error: " << err.what() << std::endl;
+	}
+	catch(...) {
+		std::cout << "Unknown error has occurred." << std::endl;
+	}
+	
+	std::cout << std::endl;
 	return 0;
 }
 
