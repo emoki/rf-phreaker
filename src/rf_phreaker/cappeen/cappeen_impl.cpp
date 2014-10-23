0b6561229e4f2bb78b915f16547410aca9f6287c
ethan steffens <ethan.steffens@gmail.com>
2014-10-22 23:32:52 -0400

    Version bump: cappeen_api_v0.11.15.0.

--- src/rf_phreaker/cappeen/cappeen_impl.cpp
+++ src/rf_phreaker/cappeen/cappeen_impl.cpp
@@ -639,5 +639,5 @@ long cappeen_impl::input_new_license(const char *serial, uint32_t serial_buf_siz
 
 const char* cappeen_impl::api_version() const
 {
-	return "0.11.13.0";
+	return "0.11.15.0";
 }
