15456c692fd224b5a0189fd513ec2308f1407eec
ethan steffens <ethan.steffens@gmail.com>
2015-07-05 22:21:03 -0400

    Added fft_shift, inverse_fft_shift, initialization function to fft_helper.

--- src/rf_phreaker/common/fft_helper.cpp
+++ src/rf_phreaker/common/fft_helper.cpp
@@ -2,20 +2,37 @@
 
 namespace rf_phreaker {
 
+fft_helper::fft_helper()
+	: fft_spec_(nullptr)
+	, fft_order_(0)
+	, flag_(0) {}
+
 fft_helper::fft_helper(int fft_order, int flag)
-: fft_spec_(0)
-{
+	: fft_spec_(nullptr) {
 	reset(fft_order, flag);
 }
 
-
-fft_helper::~fft_helper(void)
-{
+fft_helper::~fft_helper(void) {
+	if(fft_spec_)
 		ipp_helper::check_status(ippsFFTFree_C_32fc(fft_spec_));
 }
 
-void fft_helper::reset(int fft_order, int flag)
-{
+void fft_helper::reset_based_on_signal_length(int signal_length) {
+	if(signal_length <= 0) 
+		throw ipp_error("Unable to determine order for FFT.  Signal length is invalid.");
+
+	int order = static_cast<int>(log10(signal_length) / log10(2.0));
+	
+	if(abs(pow(2, order) - signal_length) > .0001)
+		throw ipp_error("Zero padding the fft is not currently supported.  Signal length must be changed to match order.");
+
+	reset(order, IPP_FFT_DIV_FWD_BY_N);
+}
+
+void fft_helper::reset(int fft_order, int flag) {
+	if(fft_order_ == fft_order && flag == flag_ && fft_spec_ != nullptr)
+		return;
+
 	flag_ = flag;
 	fft_order_ = fft_order;
 
@@ -38,14 +55,44 @@ void fft_helper::reset(int fft_order, int flag)
 	internal_buffer_.reset(buf_size);
 }
 
-void fft_helper::fft_forward(ipp_32fc_array &src, ipp_32fc_array &dst)
-{
+void fft_helper::fft_forward(const ipp_32fc_array &src, ipp_32fc_array &dst) {
 	ipp_helper::check_status(ippsFFTFwd_CToC_32fc(src.get(), dst.get(), fft_spec_, internal_buffer_.get()));
 }
 
-void fft_helper::fft_inverse(ipp_32fc_array &src, ipp_32fc_array &dst)
-{
+void fft_helper::fft_inverse(const ipp_32fc_array &src, ipp_32fc_array &dst) {
 	ipp_helper::check_status(ippsFFTInv_CToC_32fc(src.get(), dst.get(), fft_spec_, internal_buffer_.get()));
+}
+
+void fft_helper::fft_shift(ipp_32fc_array &src_dst) {
+	int tmp_length = (int)(src_dst.length() / 2.0);
+
+	// Account for odd array sizes.
+	int odd_component = 0;
+	if(tmp_length - src_dst.length() / 2.0 > .5)
+		odd_component = 1;
+
+	if(tmp_length + 1 > fft_shift_buffer_.length())
+		fft_shift_buffer_.reset(tmp_length + 1);
+
+	ipp_helper::check_status(ippsCopy_32fc(src_dst.get(), fft_shift_buffer_.get(), tmp_length + odd_component));
+	ipp_helper::check_status(ippsCopy_32fc((src_dst.get() + tmp_length + odd_component), src_dst.get(), tmp_length));
+	ipp_helper::check_status(ippsCopy_32fc(fft_shift_buffer_.get(), src_dst.get() + tmp_length + odd_component, tmp_length + odd_component));
+}
+
+void fft_helper::inverse_fft_shift(ipp_32fc_array &src_dst) {
+	int tmp_length = (int)(src_dst.length() / 2.0);
+
+	// Account for odd array sizes.
+	int odd_component = 0;
+	if(tmp_length - src_dst.length() / 2.0 > .5)
+		odd_component = 1;
+
+	if(tmp_length + 1 > fft_shift_buffer_.length())
+		fft_shift_buffer_.reset(tmp_length + 1);
+
+	ipp_helper::check_status(ippsCopy_32fc(src_dst.get(), fft_shift_buffer_.get(), tmp_length));
+	ipp_helper::check_status(ippsCopy_32fc((src_dst.get() + tmp_length), src_dst.get(), tmp_length + odd_component));
+	ipp_helper::check_status(ippsCopy_32fc(fft_shift_buffer_.get(), src_dst.get() + tmp_length, tmp_length));
 }
 
 }
--- src/rf_phreaker/common/fft_helper.h
+++ src/rf_phreaker/common/fft_helper.h
@@ -1,21 +1,32 @@
 #pragma once
 
+#include <vector>
 #include "rf_phreaker/common/ipp_array.h"
+#include "rf_phreaker/common/common_types.h"
+#include "rf_phreaker/common/moving_window_calculator.h"
 
 namespace rf_phreaker {
 
 class fft_helper
 {
 public:
-	fft_helper(int fft_order = 0, int flag = IPP_FFT_DIV_BY_SQRTN);
+	fft_helper();
+
+	fft_helper(int fft_order, int flag = IPP_FFT_DIV_BY_SQRTN);
 
 	~fft_helper();
 
+	void reset_based_on_signal_length(int signal_length);
+
 	void reset(int fft_order, int flag);
 
-	void fft_forward(ipp_32fc_array &src, ipp_32fc_array &dst);
+	void fft_forward(const ipp_32fc_array &src, ipp_32fc_array &dst);
 
-	void fft_inverse(ipp_32fc_array &src, ipp_32fc_array &dst);
+	void fft_inverse(const ipp_32fc_array &src, ipp_32fc_array &dst);
+
+	void fft_shift(ipp_32fc_array &src_dst);
+	
+	void inverse_fft_shift(ipp_32fc_array &src_dst);
 
 private:
 	int fft_order_;
@@ -25,6 +36,8 @@ private:
 	IppsFFTSpec_C_32fc *fft_spec_; 
 
 	ipp_array<Ipp8u> internal_buffer_; 
+
+	ipp_32fc_array fft_shift_buffer_;
 };
 
 }
