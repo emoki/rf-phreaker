003dbe894221138efeaadabc0e341cacb9ef233a
ethan steffens <ethan.steffens@gmail.com>
2015-07-05 22:25:02 -0400

    frequency_bin_calculator.gtests.cpp.

---
+++ src/rf_phreaker/common_tests/frequency_bin_calculator_gtests.cpp
@@ -0,0 +1,24 @@
+#include "gtest/gtest.h"
+#include "rf_phreaker/common/frequency_bin_calculator.h"
+#include "rf_phreaker/common/frequency_shifter.h"
+#include "rf_phreaker/common/common_utility.h"
+
+using namespace rf_phreaker;
+
+TEST(FftHelper, TestMain) {
+	frequency_bin_calculator calc;
+
+	frequency_type sampling_rate = 4875000;
+
+	ipp_32fc_array signal((int)pow(2, 18));
+
+	frequency_shifter shifter((double)sampling_rate);
+
+	shifter.generate_tone(signal, signal.length(), khz(100));
+
+	calc.calculate_power_in_bins(signal, sampling_rate, khz(200));
+
+	calc.get_power_in_bin(khz(100));
+
+	calc.get_bins().output_matlab_compatible_array("test_fft.txt");
+}
