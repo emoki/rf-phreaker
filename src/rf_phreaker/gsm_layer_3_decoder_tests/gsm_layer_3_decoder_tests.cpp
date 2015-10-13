326367024435d33ba91ad5fbff80f4a8951fa4f7
ethan steffens <ethan.steffens@gmail.com>
2015-10-13 09:39:29 -0400

    Incorporated new debug IO for gsm layer 3 decode tests.

--- src/rf_phreaker/gsm_layer_3_decoder_tests/gsm_layer_3_decoder_tests.cpp
+++ src/rf_phreaker/gsm_layer_3_decoder_tests/gsm_layer_3_decoder_tests.cpp
@@ -1,11 +1,14 @@
 // test_gsm_layer_3_decoder.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
 #include <stdint.h>
-#include "rf_phreaker/layer_3_common/gsm_layer_3_message_aggregate.h"
-#include "rf_phreaker/gsm_layer_3_decoder/gsm_layer_3_decoder.h"
+#include <fstream>
+#include <string>
 #include <boost/assert.hpp>
+#include "rf_phreaker/layer_3_common/gsm_layer_3_message_aggregate.h"
+#include "rf_phreaker/layer_3_common/gsm_layer_3_message_aggregate_io.h"
+#include "rf_phreaker/gsm_layer_3_decoder/gsm_layer_3_decoder.h"
+#include "rf_phreaker/gsm_layer_3_library/gsm_bit_stream.h"
 
 // 49061B2B97130062CC17C8000A59604479800080208043
 const uint8_t valid_bit_stream_1[] = { 0x49, 0x06, 0x1B, 0x2B, 0x97, 0x13, 0x00, 0x62, 0xCC, 0x17, 0xC8, 0x00, 0x0A, 0x59, 0x60, 0x44, 0x79, 0x80, 0x00, 0x80, 0x20, 0x80, 0x43 };
@@ -17,10 +20,50 @@ const uint8_t invalid_bit_stream_1[] = { 0x55, 0x06, 0x19, 0x8F, 0x19, 0xA0, 0x0
 
 int main(int argc, char* argv[])
 {
-	layer_3_information::gsm_layer_3_message_aggregate message;
+	// We are using a relative path to load the test files.  It follows the convention of a out-of-source build using cmake.  
+	// The build directory is located on the same level as the git root directory.  The git root directory's name is lte_phreaker
+	// and the test_files are located within test_files. So the directory struct would look like:
+	// ./cmake_build_directory			(cmake build directory)
+	// ./lte_phreaker					(git root directory)
+	// ./lte_phreaker/test_files		(directory containing test data)
+	std::string folder_path =
+		"../../../../rf_phreaker/test_files/cobham10/"
+		;
+
+	std::string prefix =
+		"gsm_bit_streams3"
+		;
+
+	std::string suffix =
+		//".bin";
+		".txt";
 
 	gsm_layer_3_decoder decoder;
 
+	auto filename = folder_path + prefix + suffix;
+	std::ifstream f(filename.c_str());
+
+	for(auto i = 0; i < 10000; ++i) {
+		gsm_bit_stream_mem stream;
+		f >> stream;
+		if(!f)
+			break;
+
+		layer_3_information::gsm_layer_3_message_aggregate message;
+		auto status = decoder.decode_bcch_message(stream.bit_stream(), stream.num_bytes_in_bit_stream(), stream.num_bits_unused_in_bit_stream(), message);
+
+		BOOST_ASSERT(status == 0);
+		
+		static std::ofstream out("gsm_layer_3.txt");
+		static auto out_header = false;
+		if(!out_header) {
+			layer_3_information::header(out, message) << std::endl;
+			out_header = true;
+		}
+		out << i << "\t" << message << std::endl;
+	}
+	
+	layer_3_information::gsm_layer_3_message_aggregate message;
 	int32_t status = decoder.decode_bcch_message(valid_bit_stream_1, 23, 0, message);
 	BOOST_ASSERT(status == 0);
 
--- src/rf_phreaker/gsm_layer_3_library/gsm_bit_stream.h
+++ src/rf_phreaker/gsm_layer_3_library/gsm_bit_stream.h
@@ -3,10 +3,14 @@
 #include <stdint.h>
 #include <cmath>
 #include <iomanip>
+#include <fstream>
+#include <vector>
 #include <boost/lexical_cast.hpp>
 
 class gsm_bit_stream;
+class gsm_bit_stream_mem;
 inline std::ostream& operator<<(std::ostream &os, const gsm_bit_stream &t);
+inline std::istream& operator>>(std::ostream &os, gsm_bit_stream_mem &t);
 
 class gsm_bit_stream
 {
@@ -17,6 +21,12 @@ public:
 		, bits_unused_(bits_unused)
 		, bits_consumed_thus_far_(bits_consumed_thus_far)
 	{}
+
+	gsm_bit_stream() 
+		: bit_stream_(0)
+		, num_bytes_(0)
+		, bits_unused_(0)
+		, bits_consumed_thus_far_(0) {}
 
 	const uint8_t* current_position_in_bit_stream() const { return &bit_stream_[bytes_consumed_thus_far()]; }
 
@@ -42,23 +52,57 @@ public:
 
 	const uint8_t *bit_stream() const { return bit_stream_; }
 
-protected:
+	void set_bit_stream(const uint8_t* bit_stream, uint32_t num_of_bytes, uint32_t unused_bits = 0) {
+		bit_stream_ = bit_stream;
+		num_bytes_ = num_of_bytes;
+		bits_unused_ = unused_bits;
+	}
 
+protected:
 	const uint8_t *bit_stream_;
 
-	const uint32_t num_bytes_;
+	uint32_t num_bytes_;
 
-	const uint32_t bits_unused_;
+	uint32_t bits_unused_;
 
 	uint32_t bits_consumed_thus_far_;
 };
 
+class gsm_bit_stream_mem : public gsm_bit_stream {
+public:
+	gsm_bit_stream_mem(const uint8_t *bit_stream, uint32_t num_bytes, uint32_t bits_unused, uint32_t bits_consumed_thus_far = 0)
+		: gsm_bit_stream(bit_stream, num_bytes, bits_unused, bits_consumed_thus_far) {}
+
+	gsm_bit_stream_mem() {}
+
+	friend std::istream& operator>>(std::istream &os, gsm_bit_stream_mem &t);
+
+protected:
+	std::vector<uint8_t> mem_;
+};
 
 inline std::ostream& operator<<(std::ostream &os, const gsm_bit_stream &t) {
-	os << t.num_bytes_in_bit_stream() << "\t" << t.num_bits_unused_in_bit_stream() << "\t";
+	os << std::hex << t.num_bytes_in_bit_stream() << "\t" << t.num_bits_unused_in_bit_stream() << "\t";
 
 	for(uint32_t i = 0, end = t.num_bytes_in_bit_stream(); i < end; ++i)
-		os << std::hex << std::setw(2) << std::setfill('0') << (int)t.bit_stream()[i] << " ";
+		os << std::setw(2) << std::setfill('0') << (int)t.bit_stream()[i] << " ";
 
 	return os;
+}
+
+inline std::istream& operator>>(std::istream &is, gsm_bit_stream_mem &t) {
+	uint32_t num_bytes = 0, num_bites_unused = 0;
+	is >> std::hex >> num_bytes;
+	is >> num_bites_unused;
+	if(num_bytes) {
+		t.mem_.resize(num_bytes);
+		t.set_bit_stream(&t.mem_[0], num_bytes, num_bites_unused);
+
+		for(uint32_t i = 0, end = t.num_bytes_in_bit_stream(); i < end; ++i) {
+			int j = 0;
+			is >> j;
+			t.mem_[i] = j;
+		}
+	}
+	return is;
 }
