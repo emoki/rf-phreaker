217fd057d3d3cb42d4f9a0bc27e2cbe0aa4130ec
ethan steffens <ethan.steffens@gmail.com>
2015-12-16 11:09:21 -0500

    Added collection_scheduler.
    collection_scheduler determines the next packet to collect.  It has three scheduling methodologies.
    - tech_based: Collects all freqs in one tech before moving to the next tech.  Priority is essential based on how the containers are originally ordered.
    - packet_based: Iterates thru over all techs based on given priorities.
    - collection_round_based: Iterates thru over all techs based on given priorities that are modified by the number of freqs each container has.

--- src/rf_phreaker/processing/collection_manager_body.h
+++ src/rf_phreaker/processing/collection_manager_body.h
@@ -2,6 +2,7 @@
 #include "rf_phreaker/processing/collection_info_container.h"
 #include "rf_phreaker/processing/node_defs.h"
 #include "rf_phreaker/processing/scanner_io.h"
+#include "rf_phreaker/processing/collection_scheduler.h"
 #include "rf_phreaker/common/common_types.h"
 #include "rf_phreaker/common/common_utility.h"
 #include "rf_phreaker/common/delegate_sink.h"
@@ -17,22 +18,38 @@ namespace rf_phreaker { namespace processing {
 class collection_manager_body
 {
 public:
+	collection_manager_body(const collection_manager_body &a) 
+		: scanner_interface_(a.scanner_interface_)
+		, graph_(a.graph_)
+		, containers_(a.containers_)
+		, packet_output_settings_(a.packet_output_settings_)
+		, settings_(a.settings_)
+		, gsm_sweep_(a.gsm_sweep_)
+		, gsm_layer_3_(a.gsm_layer_3_)
+		, umts_sweep_(a.umts_sweep_)
+		, umts_layer_3_(a.umts_layer_3_)
+		, lte_sweep_(a.lte_sweep_)
+		, lte_layer_3_(a.lte_layer_3_)
+		, timestamp_(a.timestamp_)
+		, scheduler_(&containers_, &settings_, a.scheduler_.has_multiple_scans())
+		, token_count_(0) {}
+
+
 	collection_manager_body(tbb::flow::graph *graph, rf_phreaker::scanner::scanner_controller_interface *sc, const collection_info_containers &info, 
-		const output_settings &p_output, const settings &s_settings)
-		: scanner_io_(sc)
-		, scanner_interface_(sc)
+		const output_settings &p_output, const settings &s_settings, bool do_multiple_scans = true)
+		: scanner_interface_(sc)
 		, graph_(graph)
 		, containers_(info)
-		, do_multiple_scans_(true)
 		, packet_output_settings_(p_output)
 		, settings_(s_settings)
+		, scheduler_(&containers_, &settings_, do_multiple_scans)
 		, gsm_sweep_(0)
 		, gsm_layer_3_(0)
 		, umts_sweep_(0)
 		, umts_layer_3_(0)
 		, lte_sweep_(0)
 		, lte_layer_3_(0)
-
+		, token_count_(0)
 	{}
 
 	void operator()(add_remove_collection_info info, collection_manager_node::output_ports_type &out)
@@ -42,95 +59,92 @@ public:
 				c.adjust(info);
 		}
 
-		bool scanning_finished_for_all_containers = true;
-
-		for(auto &c : containers_) {
-			if(!c.is_finished()) {
-				collection_info ci;
-				if(c.has_specifier(GSM_LAYER_3_DECODE)) 
-					c.current_info_determine_collection_round(ci);
-				else
-					ci = c.try_get_next_info();
-
-				if(c.is_finished()) {
-					//processing_.store(false);
-					//return;
-					continue;
-				}
-				else {
-					scanning_finished_for_all_containers = false;
-
-					//measurement_package package(std::make_shared<scanner::measurement_info>(
-					//	scanner_interface_->get_rf_data(ci.freq_, ci.time_ns_, ci.bandwidth_, ci.sampling_rate_).get()), ci.can_remove_);
-
-					std::shared_ptr<scanner::measurement_info> meas_ptr;
-					if(ci.specs_.has_spec(GSM_LAYER_3_DECODE))
-						meas_ptr = std::make_shared<scanner::measurement_info>(scanner_interface_->stream_rf_data(ci.freq_, ci.time_ns_, 35000000, ci.bandwidth_, ci.sampling_rate_).get());
-					else
-						meas_ptr = std::make_shared<scanner::measurement_info>(scanner_interface_->get_rf_data(ci.freq_, ci.time_ns_, ci.bandwidth_, ci.sampling_rate_).get());
-
-					measurement_package package(meas_ptr, ci.can_remove_);
-
-					auto &meas = package.measurement_info_;
-
-					// Make necessary changes to the packet.
-					meas->collection_round(c.collection_round());
-					meas->set_operating_bands(ci.operating_bands_);
-					if(!settings_.use_rf_board_adjustment_)
-						meas->rf_board_adjustments(scanner::rf_adjustment{});
-
-					if(ci.specs_.has_spec(GSM_SWEEP)) {
-						std::get<GSM_SWEEP_PORT>(out).try_put(package);
-						if(packet_output_settings_.gsm_sweep_) { output(meas.get(), "gsm_sweep_", gsm_sweep_++); }
-					}
-					if(ci.specs_.has_spec(GSM_LAYER_3_DECODE)) {
-						std::get<GSM_LAYER3_PORT>(out).try_put(package);
-						if(packet_output_settings_.gsm_layer_3_) { output(meas.get(), "gsm_layer_3_", gsm_layer_3_++); }
-					}
-					if(ci.specs_.has_spec(UMTS_SWEEP)) {
-						std::get<UMTS_SWEEP_PORT>(out).try_put(package);
-						if(packet_output_settings_.umts_sweep_) { output(meas.get(), "umts_sweep_", umts_sweep_++); }
-					}
-					if(ci.specs_.has_spec(UMTS_LAYER_3_DECODE)) {
-						std::get<UMTS_LAYER3_PORT>(out).try_put(package);
-						if(packet_output_settings_.umts_layer_3_) { output(meas.get(), "umts_layer_3_", umts_layer_3_++); }
-					}
-					if(ci.specs_.has_spec(LTE_SWEEP)) {
-						std::get<LTE_SWEEP_PORT>(out).try_put(package);
-						if(packet_output_settings_.lte_sweep_) { output(meas.get(), "lte_sweep_", lte_sweep_++); }
-					}
-					if(ci.specs_.has_spec(LTE_LAYER_3_DECODE)) {
-						std::get<LTE_LAYER3_PORT>(out).try_put(package);
-						if(packet_output_settings_.lte_layer_3_) { output(meas.get(), "lte_layer_3_", lte_layer_3_++); }
-					}
-
-				}
-				if(!settings_.simultaneous_collection_)
-					break;
-			}
+		// The flow graph controls processing by alloting x number of tokens to the system.  If the system is 
+		// alloted 8 tokens then 8 tasks can be occurring simultaneously.  For this to work correctly a token 
+		// cannot mutliple, meaning 1 token does not become 3 tokens.  This is what happens if we're using 
+		// the generic sweep.  One token comes in and it's possible to send three out (gsm, umts, lte).  Therefore 
+		// we keep a count of how many tokens we sent out and wait until we have received that number of tokens 
+		// before collecting another packet.  Because this is the first node in the system we should be able to stay
+		// under the number of tokens alloted.
+		if(--token_count_ > 0) {
+			return;
 		}
 
-		if(scanning_finished_for_all_containers) {
-			if(do_multiple_scans_) {
-				for(auto &c : containers_)
-					c.reset();
-			}
-			else
-				finished_scanning();
+		scheduler_.update();
 
+		collection_info ci;
+		if(scheduler_.get_next_collection_info(ci)) {
+			std::shared_ptr<scanner::measurement_info> meas_ptr;
+			if(ci.specs_.has_spec(GSM_LAYER_3_DECODE))
+				meas_ptr = std::make_shared<scanner::measurement_info>(scanner_interface_->stream_rf_data(ci.freq_, ci.time_ns_, 35000000, ci.bandwidth_, ci.sampling_rate_).get());
+			else
+				meas_ptr = std::make_shared<scanner::measurement_info>(scanner_interface_->get_rf_data(ci.freq_, ci.time_ns_, ci.bandwidth_, ci.sampling_rate_).get());
+
+			measurement_package package(meas_ptr, ci.can_remove_);
+
+			auto &meas = package.measurement_info_;
+
+			// Make necessary changes to the packet.
+			meas->collection_round(scheduler_.current_container_collection_round());
+			meas->set_operating_bands(ci.operating_bands_);
+			if(!settings_.use_rf_board_adjustment_)
+				meas->rf_board_adjustments(scanner::rf_adjustment{});
+
+			if(ci.specs_.has_spec(GSM_SWEEP)) {
+				//if(std::get<GSM_SWEEP_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<GSM_SWEEP_PORT>(out, package);
+				if(packet_output_settings_.gsm_sweep_) { output(meas.get(), "gsm_sweep_", gsm_sweep_++); }
+			}
+			if(ci.specs_.has_spec(GSM_LAYER_3_DECODE)) {
+				//if(std::get<GSM_LAYER3_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<GSM_LAYER3_PORT>(out, package);
+				if(packet_output_settings_.gsm_layer_3_) { output(meas.get(), "gsm_layer_3_", gsm_layer_3_++); }
+			}
+			if(ci.specs_.has_spec(UMTS_SWEEP)) {
+				//if(std::get<UMTS_SWEEP_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<UMTS_SWEEP_PORT>(out, package);
+				if(packet_output_settings_.umts_sweep_) { output(meas.get(), "umts_sweep_", umts_sweep_++); }
+			}
+			if(ci.specs_.has_spec(UMTS_LAYER_3_DECODE)) {
+				//if(std::get<UMTS_LAYER3_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<UMTS_LAYER3_PORT>(out, package);
+				if(packet_output_settings_.umts_layer_3_) { output(meas.get(), "umts_layer_3_", umts_layer_3_++); }
+			}
+			if(ci.specs_.has_spec(LTE_SWEEP)) {
+				//if(std::get<LTE_SWEEP_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<LTE_SWEEP_PORT>(out, package);
+				if(packet_output_settings_.lte_sweep_) { output(meas.get(), "lte_sweep_", lte_sweep_++); }
+			}
+			if(ci.specs_.has_spec(LTE_LAYER_3_DECODE)) {
+				//if(std::get<LTE_LAYER3_PORT>(out).try_put(package)) ++token_count_;
+				send_msg<LTE_LAYER3_PORT>(out, package);
+				if(packet_output_settings_.lte_layer_3_) { output(meas.get(), "lte_layer_3_", lte_layer_3_++); }
+			}
+		}
+		else {
 			// If scanning has finished for all containers then we will not process any packets this time thru which 
 			// means we need to send a continue msg to the limiter node directly.
-			std::get<LIMITER_PORT>(out).try_put(tbb::flow::continue_msg());
+			//if(std::get<LIMITER_PORT>(out).try_put(tbb::flow::continue_msg())) ++token_count_;
+			send_msg<LIMITER_PORT>(out, tbb::flow::continue_msg());
 		}
+
+		if(scheduler_.is_done())
+			finished_scanning();
 	}
 
 protected:
+	template<int Port, typename Msg>
+	void send_msg(collection_manager_node::output_ports_type &out, Msg &msg) {
+		if(std::get<Port>(out).try_put(msg))
+			++token_count_;
+	}
+	
 	void output(scanner::measurement_info *meas, std::string name, int count)
 	{
-		if(timestamp.empty())
-			timestamp = static_timestamp::to_string();
+		if(timestamp_.empty())
+			timestamp_ = static_timestamp::to_string();
 		std::string ext = settings_.output_in_binary_ ? ".bin" : ".txt";
-		std::ofstream file(settings_.output_directory_ + "\\" + name + timestamp + "_" + std::to_string(count) + ext,
+		std::ofstream file(settings_.output_directory_ + "\\" + name + timestamp_ + "_" + std::to_string(count) + ext,
 			settings_.output_in_binary_ ? std::ios::binary : std::ios::app);
 		if(file) {
 			if(settings_.output_in_binary_) {
@@ -146,15 +160,11 @@ protected:
 
 	virtual void finished_scanning() { throw processing_error("Finished scanning not supported."); }
 
-	scanner_io scanner_io_;
-	
 	rf_phreaker::scanner::scanner_controller_interface *scanner_interface_;
 
 	tbb::flow::graph *graph_;
 
 	collection_info_containers containers_;
-
-	bool do_multiple_scans_;
 
 	output_settings packet_output_settings_;
 	settings settings_;
@@ -165,7 +175,11 @@ protected:
 	int lte_sweep_;
 	int lte_layer_3_;
 
-	std::string timestamp;
+	std::string timestamp_;
+
+	collection_scheduler scheduler_;
+
+	int token_count_;
 };
 
 class freq_correction_collection_manager_body : public collection_manager_body
@@ -173,10 +187,8 @@ class freq_correction_collection_manager_body : public collection_manager_body
 public:
 	freq_correction_collection_manager_body(tbb::flow::graph *graph, rf_phreaker::scanner::scanner_controller_interface *sc, const collection_info_containers &info, 
 		const output_settings &p_output, const settings &s_settings)
-	: collection_manager_body(graph, sc, info, p_output, s_settings)
-	{
-		do_multiple_scans_ = false;
-	}
+	: collection_manager_body(graph, sc, info, p_output, s_settings, false)
+	{}
 
 protected:
 	void finished_scanning() 
---
+++ src/rf_phreaker/processing_tests/collection_scheduler_gtests.cpp
@@ -0,0 +1,332 @@
+#include "gtest/gtest.h"
+
+#include "rf_phreaker/processing/collection_info_container.h"
+#include "rf_phreaker/processing/collection_scheduler.h"
+#include "rf_phreaker/common/common_utility.h"
+#include "rf_phreaker/common/common_types.h"
+#include "rf_phreaker/common/benchmark.h"
+#include "rf_phreaker/common/settings.h"
+
+using namespace rf_phreaker;
+using namespace rf_phreaker::processing;
+
+class CollectionScheduler : public ::testing::Test
+{
+protected:
+	CollectionScheduler() 
+		: c1(rf_phreaker::UMTS_LAYER_3_DECODE, false, false) 
+		, c2(rf_phreaker::LTE_LAYER_3_DECODE, false, false) 
+		, c3(rf_phreaker::GSM_LAYER_3_DECODE, false, false)
+		, c3_empty(rf_phreaker::GSM_LAYER_3_DECODE, false, false)
+		, c4(rf_phreaker::GSM_SWEEP, false, false) {
+		info_c1a = collection_info(mhz(11), 10000, mhz(5), 0, UMTS_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2a = collection_info(mhz(21), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2b = collection_info(mhz(22), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2c = collection_info(mhz(23), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2d = collection_info(mhz(24), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2e = collection_info(mhz(25), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c2f = collection_info(mhz(26), 10000, mhz(5), 0, LTE_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c3a = collection_info(mhz(31), 10000, mhz(5), 0, GSM_LAYER_3_DECODE, OPERATING_BAND_UNKNOWN, true);
+		info_c4a = collection_info(mhz(41), 10000, mhz(5), 0, GSM_SWEEP, OPERATING_BAND_UNKNOWN, true);
+		info_c4b = collection_info(mhz(42), 10000, mhz(5), 0, GSM_SWEEP, OPERATING_BAND_UNKNOWN, true);
+		info_c4c = collection_info(mhz(43), 10000, mhz(5), 0, GSM_SWEEP, OPERATING_BAND_UNKNOWN, true);
+		info_c4d = collection_info(mhz(44), 10000, mhz(5), 0, GSM_SWEEP, OPERATING_BAND_UNKNOWN, true);
+		info_c4e = collection_info(mhz(45), 10000, mhz(5), 0, GSM_SWEEP, OPERATING_BAND_UNKNOWN, true);
+
+		c1.adjust(add_collection_info(info_c1a));
+		c2.adjust(add_collection_info(info_c2a));
+		c2.adjust(add_collection_info(info_c2b));
+		c2.adjust(add_collection_info(info_c2c));
+		c2.adjust(add_collection_info(info_c2d));
+		c2.adjust(add_collection_info(info_c2e));
+		c2.adjust(add_collection_info(info_c2f));
+		c3.adjust(add_collection_info(info_c3a));
+		c4.adjust(add_collection_info(info_c4a));
+		c4.adjust(add_collection_info(info_c4b));
+		c4.adjust(add_collection_info(info_c4c));
+		c4.adjust(add_collection_info(info_c4d));
+		c4.adjust(add_collection_info(info_c4e));
+	}
+	collection_info_container c1;
+	collection_info_container c2;
+	collection_info_container c3;
+	collection_info_container c4;
+
+	collection_info_container c3_empty;
+
+	collection_info info_c1a;
+	collection_info info_c2a;
+	collection_info info_c2b;
+	collection_info info_c2c;
+	collection_info info_c2d;
+	collection_info info_c2e;
+	collection_info info_c2f;
+	collection_info info_c3a;
+	collection_info info_c4a;
+	collection_info info_c4b;
+	collection_info info_c4c;
+	collection_info info_c4d;
+	collection_info info_c4e;
+
+	bool verify(const std::vector<collection_info> &g, collection_info_container &con, int num) {
+		bool is_good = true;
+		auto count = 0;
+		auto it = con.collection_info_group_.begin();
+		std::for_each(g.begin(), g.end(), [&](const collection_info &i) {
+			if(i.specs_.front() == con.specs_.front()) {
+				++count;
+				if(it->freq_ != i.freq_)
+					is_good = false;
+				if(++it == con.collection_info_group_.end())
+					it = con.collection_info_group_.begin();
+			}
+		});
+		return is_good && count == num;
+	}
+};
+
+TEST_F(CollectionScheduler, TestTechBased) {
+
+	collection_info_containers containers;
+	containers.push_back(c1);
+	c2.finish_after_iteration_ = true;
+	containers.push_back(c2);
+	containers.push_back(c3);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 4;
+	s.lte_layer_3_collection_.priority_ = 16;
+	s.scheduling_algorithm_ = tech_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	collection_info info;
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c1a, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c1a, info);
+
+	containers[0].adjust(remove_collection_info(info_c1a));
+
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2a, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2b, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2c, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2d, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2e, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2f, info);
+
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c3a, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c3a, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c3a, info);
+
+	containers[2].adjust(remove_collection_info(info_c3a));
+
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2a, info);
+
+	EXPECT_EQ(false, scheduler.is_done());
+}
+
+TEST_F(CollectionScheduler, TestTechBasedIsDone) {
+
+	collection_info_containers containers;
+	c2.finish_after_iteration_ = true;
+	containers.push_back(c2);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 4;
+	s.lte_layer_3_collection_.priority_ = 16;
+	s.scheduling_algorithm_ = tech_based;
+
+	collection_scheduler scheduler(&containers, &s, false);
+	collection_info info;
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2a, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2b, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2c, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2d, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2e, info);
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(info_c2f, info);
+
+
+	EXPECT_EQ(false, scheduler.is_done());
+	scheduler.get_next_collection_info(info);
+	EXPECT_EQ(true, scheduler.is_done());
+}
+
+TEST_F(CollectionScheduler, TestPacketBasedSamePriorityExhaustive) {
+	collection_info_containers containers;
+	containers.push_back(c1);
+	containers.push_back(c2);
+	containers.push_back(c3);
+	containers.push_back(c4);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 1;
+	s.lte_layer_3_collection_.priority_ = 1;
+	s.gsm_layer_3_collection_.priority_ = 1;
+	s.gsm_sweep_collection_.priority_ = 1;
+	s.scheduling_algorithm_ = packet_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 24; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c1, 6));
+	EXPECT_TRUE(verify(group, c2, 6));
+	EXPECT_TRUE(verify(group, c3, 6));
+	EXPECT_TRUE(verify(group, c4, 6));
+}
+
+TEST_F(CollectionScheduler, TestPacketBasedDiffPriority) {
+	collection_info_containers containers;
+	containers.push_back(c1);
+	containers.push_back(c2);
+	containers.push_back(c3);
+	containers.push_back(c4);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 10;
+	s.lte_layer_3_collection_.priority_ = 5;
+	s.gsm_layer_3_collection_.priority_ = 7;
+	s.gsm_sweep_collection_.priority_ = 1;
+	s.scheduling_algorithm_ = packet_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 23; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c1, 10));
+	EXPECT_TRUE(verify(group, c2, 5));
+	EXPECT_TRUE(verify(group, c3, 7));
+	EXPECT_TRUE(verify(group, c4, 1));
+}
+
+TEST_F(CollectionScheduler, TestPacketBasedDifferentPriority) {
+	collection_info_containers containers;
+	containers.push_back(c1);
+	containers.push_back(c2);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 4;
+	s.lte_layer_3_collection_.priority_ = 16;
+	s.scheduling_algorithm_ = packet_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 6; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c1, 2));
+	EXPECT_TRUE(verify(group, c2, 4));
+}
+
+TEST_F(CollectionScheduler, TestCRBasedSamePriority) {
+	collection_info_containers containers;
+	containers.push_back(c1);
+	containers.push_back(c2);
+	containers.push_back(c3);
+	containers.push_back(c4);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 1;
+	s.lte_layer_3_collection_.priority_ = 1;
+	s.gsm_layer_3_collection_.priority_ = 1;
+	s.gsm_sweep_collection_.priority_ = 1;
+	s.scheduling_algorithm_ = collection_round_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 9; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c1, 1));
+	EXPECT_TRUE(verify(group, c2, 6));
+	EXPECT_TRUE(verify(group, c3, 1));
+	EXPECT_TRUE(verify(group, c4, 1));
+}
+
+TEST_F(CollectionScheduler, TestCRBasedDiffPriority) {
+	collection_info_containers containers;
+	containers.push_back(c1);
+	containers.push_back(c2);
+	containers.push_back(c3);
+	containers.push_back(c4);
+
+	settings s;
+	s.umts_layer_3_collection_.priority_ = 5;
+	s.lte_layer_3_collection_.priority_ = 1;
+	s.gsm_layer_3_collection_.priority_ = 1;
+	s.gsm_sweep_collection_.priority_ = 5;
+	s.scheduling_algorithm_ = collection_round_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 17; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c1, 5));
+	EXPECT_TRUE(verify(group, c2, 6));
+	EXPECT_TRUE(verify(group, c3, 1));
+	EXPECT_TRUE(verify(group, c4, 5));
+}
+
+TEST_F(CollectionScheduler, TestCRBasedEmptyContainer) {
+	collection_info_containers containers;
+	containers.push_back(c3_empty);
+	containers.push_back(c4);
+
+	settings s;
+	s.gsm_layer_3_collection_.priority_ = 4;
+	s.gsm_sweep_collection_.priority_ = 1;
+	s.scheduling_algorithm_ = collection_round_based;
+
+	collection_scheduler scheduler(&containers, &s);
+	std::vector<collection_info> group;
+	collection_info info;
+	for(int i = 0; i < 8; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c3, 0));
+	EXPECT_TRUE(verify(group, c4, 8));
+
+	containers[0].adjust(add_collection_info(info_c3a));
+
+	// Reset so c3 freqs match..
+	scheduler.reset();
+	scheduler.update();
+	group.clear();
+	for(int i = 0; i < 10; ++i) {
+		scheduler.get_next_collection_info(info);
+		group.push_back(info);
+	}
+	EXPECT_TRUE(verify(group, c3, 8));
+	EXPECT_TRUE(verify(group, c4, 2));
+}
+
