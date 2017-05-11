85ab9bc19a38f0e655ad6efb02685e1083eb35ca
ethan steffens <ethan.steffens@gmail.com>
2017-05-11 13:50:39 -0400

    Modified frequency conversion defines to int64_t.

--- src/rf_phreaker/common/common_utility.h
+++ src/rf_phreaker/common/common_utility.h
@@ -8,10 +8,10 @@
 #include "boost/format.hpp"
 #include "rf_phreaker/common/common_types.h"
 
-#define khz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000)             /**< Convenience for kHz */
-#define mhz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000000)          /**< Convenience for MHz */
-#define ghz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000000000)       /**< Convenience for GHz */
-#define milli_to_nano(x) (static_cast<rf_phreaker::time_type>(x)*1000000)       /**< Convenience for nano */
+#define khz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000L)             /**< Convenience for kHz */
+#define mhz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000000L)          /**< Convenience for MHz */
+#define ghz(x) (static_cast<rf_phreaker::frequency_type>(x)*1000000000L)       /**< Convenience for GHz */
+#define milli_to_nano(x) (static_cast<rf_phreaker::time_type>(x)*1000000L)       /**< Convenience for nano */
 
 
 namespace rf_phreaker {
