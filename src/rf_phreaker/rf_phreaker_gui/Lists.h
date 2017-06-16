2b045c450becc33ff39a7420a1c38cae382f8bcf
ethan steffens <ethan.steffens@gmail.com>
2017-06-16 16:52:46 -0400

    Remove old files

--- src/rf_phreaker/rf_phreaker_gui/Lists.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include "rf_phreaker/rf_phreaker_gui/WcdmaList.h"
-#include "rf_phreaker/rf_phreaker_gui/CollectionInfo.h"
-
-class CollectionInfoList1 : public QmlList<CollectionInfo> {};
